0f4daca0f1e19b1244022045b4d88f2444960088
Raksha-k-c <raksha.chandru66@gmail.com>
2026-07-26 19:37:32 +0000

    Stage 1: UTS namespace hostname isolation working

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Building a minimal container runtime from scratch to understand how Docker works under the hood.
 
 ## Progress
-- [ ] Stage 1: Hostname isolation (UTS namespace)
+- [x] Stage 1: Hostname isolation (UTS namespace)
 - [ ] Stage 2: Process isolation (PID namespace)
 - [ ] Stage 3: Filesystem isolation (chroot + mount namespace)
 - [ ] Stage 4: Resource limits (cgroups)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v as PID %d (inside new namespace)\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("mycontainer"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
